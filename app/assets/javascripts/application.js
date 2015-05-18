7b4687304958ec560b97d340f81c69b9c7394de2
Jeff Conway <jconway@arcwebtech.com>
2015-05-18 18:51:10 -0400

    Handle page sizes better

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,26 +19,33 @@
 
 $(document).ready(function(){
   
-  window_height = window.innerHeight;
+  // window_height = window.innerHeight;
 
-  header_height = $("nav").height();
-  footer_height = $("#full-footer").height();
+  // header_height = $("nav").height();
+  // footer_height = $("#full-footer").height();
 
-  remaining_height = window_height - header_height - footer_height;
+  // remaining_height = window_height - header_height - footer_height;
 
-  string_height = remaining_height + "px";
+  // string_height = remaining_height + "px";
 
-  $("#left-side").css({"min-height": string_height});
+  // $("#left-side").css({"min-height": string_height});
+
+  resize_padding_for_footer();
 
   $(window).resize(function(event) {
+    resize_padding_for_footer();
+  });
+
+  function resize_padding_for_footer(){
     if ( $("#footer").height() > 107 ){
       // something unusual is happening
-      $("#full-content").css({"padding-bottom": "300px"});
+      f_height = $("#footer").height();
+      $("#full-content").css({"padding-bottom": f_height + 80 + "px"});
     }
     else{
       // we're back to normal
       $("#full-content").css({"padding-bottom": "90px"});
     }
-  });
+  }
 
 });
